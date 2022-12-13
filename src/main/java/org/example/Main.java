93a4301283c5b86e617ea6a4c3c953519bf1c928
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-13 16:22:01 -0300

    chore: refactor Prototype pattern

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,7 @@
 package org.example;
 
-import org.example.pattern.creational.Prototype.PrototypeCard;
-import org.example.pattern.creational.Prototype.PrototypeFactory;
-import org.example.pattern.creational.Prototype.VisaCard;
-
-import java.lang.CloneNotSupportedException;
-
-import static org.example.pattern.creational.Prototype.PrototypeFactory.CardType.AMEX;
-import static org.example.pattern.creational.Prototype.PrototypeFactory.CardType.VISA;
-
 public class Main {
     public static void main(String[] args) {
-        PrototypeFactory.loadCard();
-        try {
-            PrototypeCard card = PrototypeFactory.getInstance(AMEX);
-            card.getCard();
-        } catch (CloneNotSupportedException e) {
-            System.out.println(e);
-        }
+
     }
 }
--- src/main/java/org/example/pattern/creational/Prototype/Main.java
+++ src/main/java/org/example/pattern/creational/Prototype/Main.java
@@ -1,7 +1,16 @@
 package org.example.pattern.creational.Prototype;
 
+import java.lang.CloneNotSupportedException;
+import static org.example.pattern.creational.Prototype.PrototypeFactory.CardType.AMEX;
+
 public class Main {
     public static void main(String[] args) {
-
+        PrototypeFactory.loadCard();
+        try {
+            PrototypeCard card = PrototypeFactory.getInstance(AMEX);
+            card.getCard();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
     }
 }
