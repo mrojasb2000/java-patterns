4262e3047f24a8ec7b2511c97008176be1b424b2
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-14 14:58:42 -0300

    feat: added Lambda repeat

---
+++ src/main/java/org/example/functional/Repeat/RepeatWelcome.java
@@ -0,0 +1,8 @@
+package org.example.functional.Repeat;
+
+public class RepeatWelcome {
+    public static void main(String[] args){
+        String welcome = "Welcome! ".repeat(3);
+        System.out.println(welcome);
+    }
+}
