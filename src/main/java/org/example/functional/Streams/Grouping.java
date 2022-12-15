063259444cf1f451450313865ebbc05218fa5661
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-15 16:47:49 -0300

    feat: grouping stream values

---
+++ src/main/java/org/example/functional/Streams/Grouping.java
@@ -0,0 +1,15 @@
+package org.example.functional.Streams;
+
+import java.util.stream.IntStream;
+
+public class Grouping {
+    public static void main(String[] args){
+        long startTime = System.currentTimeMillis();
+        long counted = IntStream.rangeClosed(0, 1_000_000_000)
+                .filter(i -> i % 2 == 0)
+                .count();
+        long duration = System.currentTimeMillis() - startTime;
+        System.out.printf("the number the events %d time for execution %d", counted, duration);
+
+    }
+}
