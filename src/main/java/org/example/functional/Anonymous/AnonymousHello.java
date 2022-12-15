e3a87871765791d8b1040b92255db0b5b7f65be8
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-15 14:55:26 -0300

    feat: added anonymous hello multilanguage

---
+++ src/main/java/org/example/functional/Anonymous/AnonymousHello.java
@@ -0,0 +1,17 @@
+package org.example.functional.Anonymous;
+
+interface Hello {
+    void sayHello();
+}
+
+public class AnonymousHello {
+    public static void main(String[] args){
+        Hello englishHello = () -> System.out.println("Hello");
+        Hello frenchHello = () -> System.out.println("Bonjour");
+        Hello spanishHello = () -> System.out.println("Hola");
+        englishHello.sayHello();
+        frenchHello.sayHello();
+        spanishHello.sayHello();
+    }
+}
+
