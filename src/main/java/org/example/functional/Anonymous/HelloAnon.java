756e5bb03b856db63f9264572b28b8b124221cfb
Mauricio Rojas <mrojasb2000@gmail.com>
2022-12-15 17:39:09 -0300

    refactor: change by lambda expression

--- src/main/java/org/example/functional/Anonymous/HelloAnon.java
+++ src/main/java/org/example/functional/Anonymous/HelloAnon.java
@@ -3,24 +3,9 @@ package org.example.functional.Anonymous;
 
 public class HelloAnon {
     public static void main(String[] args){
-        Hello english = new Hello() {
-            @Override
-            public void sayHello() {
-                System.out.println("Hello");
-            }
-        };
-        Hello french = new Hello() {
-            @Override
-            public void sayHello() {
-                System.out.println("Bonjour");
-            }
-        };
-        Hello spanish = new Hello() {
-            @Override
-            public void sayHello() {
-                System.out.println("Hola");
-            }
-        };
+        Hello english = () -> System.out.println("Hello");
+        Hello french = () -> System.out.println("Bonjour");
+        Hello spanish = () -> System.out.println("Hola");
 
         english.sayHello();
         french.sayHello();
